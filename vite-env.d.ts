b73572c5fb749b5b1ab4647046b19c9247ecce42
KassenkazyAlima <154450040+KassenkazyAlima@users.noreply.github.com>
2025-11-09 01:04:06 +0500

    Initial working frontend with API base and report page

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,10 +1,10 @@
-/// <reference types="vite/client" />
+
 
 interface ImportMetaEnv {
-    readonly VITE_API_BASE_URL: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
+  readonly VITE_API_BASE_URL: string;
+  // add other VITE_ variables here if you create them
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
